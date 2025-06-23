97712f83eee8d611bf63cadd194860be41e78fb6
Giovanny Azevedo <giolivaze@gmail.com>
2025-06-22 22:34:14 -0300

    Layout changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { Html5QrcodeScanner } from 'html5-qrcode';
 import styles from '../styles/Home.module.css';
+import { CircularProgress, SvgIcon } from '@mui/material';
 
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxBMewrE3OWPOa87Hk0nmstJp2Dk44yRs7O73unl7w3COEjjFsJy_O9rIiCNRbwQlt0vQ/exec'; // <<<--- IMPORTANTE!
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyqqjyXdnNRpYDHrlcXJ_Cj5Wiowj-ArKQ9NT7Rf4a40ZVS8OG1w8ZRWd98ytIKkF3OFQ/exec';
 
 export default function Home() {
-  useEffect(() => {
-    const resultDiv = document.getElementById('result') as HTMLDivElement;
+  const [status, setStatus] = useState('WAITING');
+  const [message, setMessage] = useState('Aguardando leitura...');
 
+  useEffect(() => {
     function onScanSuccess(decodedText: string) {
       html5QrcodeScanner.pause();
-      resultDiv.innerHTML = `Registrando presença para ID: ${decodedText}...`;
-      resultDiv.className = '';
+
+      setStatus('REGISTERING');
+      setMessage(`Registrando presença para ID: ${decodedText}`);
 
       fetch(SCRIPT_URL, {
         method: 'POST',
@@ -22,25 +25,18 @@ export default function Home() {
       })
         .then((res) => res.json())
         .then((data) => {
-          resultDiv.innerHTML = data.message;
-          if (data.status === 'success') {
-            resultDiv.className = styles.success;
-            navigator.vibrate?.(200);
-          } else {
-            resultDiv.className = styles.error;
-            navigator.vibrate?.([100, 50, 100]);
-          }
+          setMessage(data.message);
+          setStatus(data.status === 'success' ? 'SUCCESS' : 'ERROR');
         })
         .catch((error) => {
-          console.error('Erro:', error);
-          resultDiv.innerHTML = 'Erro ao conectar com o servidor.';
-          resultDiv.className = styles.error;
+          setStatus('ERROR');
+          setMessage('Erro ao conectar com o servidor.');
         })
         .finally(() => {
           setTimeout(() => {
             html5QrcodeScanner.resume();
-            resultDiv.innerHTML = 'Aguardando leitura...';
-            resultDiv.className = '';
+            setStatus('WAITING');
+            setMessage('Aguardando leitura...');
           }, 3000);
         });
     }
@@ -66,7 +62,41 @@ export default function Home() {
       <main className={styles.container}>
         <h1></h1>
         <div id="qr-reader" className={styles.qrBox}></div>
-        <div id="result" className={styles.result}>Aguardando leitura...</div>
+        <div id="result" className={styles.result}>
+
+          { status === "WAITING" && 
+            <div className={styles.waiting}>
+              <span>{ message }</span>
+            </div>
+          }
+
+          { status === "REGISTERING" && 
+            <div className={styles.registering}>
+              <CircularProgress thickness={5} color='inherit' size={50}/>
+              <span>{ message }</span>
+            </div>
+          }
+
+          { status === "SUCCESS" && 
+            <div className={styles.success}>
+              <svg width="71" height="71" viewBox="0 0 71 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="35.5" cy="35.5" r="35.5" fill="#D5EBDE"/>
+                <path d="M29.9098 49L20 35.7455L22.2869 33.2909L30.418 38.6909C34.4836 32.4073 43.1229 24.9455 46.9344 22H51C40.6328 32.0145 32.6202 44.1727 29.9098 49Z" fill="#5DA277"/>
+              </svg>
+              <span>{ message }</span>
+            </div>
+          }
+
+          { status === "ERROR" && 
+            <div className={styles.error}>
+              <svg width="71" height="71" viewBox="0 0 71 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="35.5" cy="35.5" r="35.5" fill="#FFC1CE"/>
+                <path d="M24.5854 47L48 24M24 24L47.4146 47" stroke="#93344A" stroke-width="5" stroke-linecap="round"/>
+              </svg>
+              <span>{ message }</span>
+            </div>
+          }
+        </div>
       </main>
     </>
   );
